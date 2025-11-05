91e9ade10f91e09bca16726384f2514865f4420a
olanate <85747642+olanate@users.noreply.github.com>
2025-11-05 14:51:22 -0700

    updated filter

--- web_scrapping3.py
+++ web_scrapping3.py
@@ -1,63 +1,58 @@
 import streamlit as st
-import requests
 from bs4 import BeautifulSoup
+import requests
 import pandas as pd
-
-def get_data(dept):
-    url = requests.get(f'https://www.{dept}.ruet.ac.bd/teacher_list').text
+ 
+def get_data(which_dept):
+    url = requests.get(f'https://www.{which_dept}.ruet.ac.bd/teacher_list').text
     soup = BeautifulSoup(url, 'lxml')
     teachers = soup.find_all('tr')[1:]
-    #print (teachers)
-    name_en = []
-    designation = []
-    email = []
-    phone_no = []
-    depart = []
-
+    names = []
+    designations = []
+    phones = []
+    emails = []
+    depts = []
     for teacher in teachers:
-        name = teacher.find_all('td')[1].text.strip()
-        desig = teacher.find_all('td')[3].text.strip()
-        phone = teacher.find_all('td')[6].text.strip()
-        em = teacher.find_all('td')[5].text.strip()
-        dept = teacher.find_all('td')[4].text.strip()
-
-        name_en.append(name)
-        designation.append(desig)
-        email.append(em)
-        phone_no.append(phone)
-        depart.append(dept)
-    
-    data = pd.DataFrame({'Name': name_en, 'Designation': designation, 'Email': email, 'Phone': phone_no, 'Department': depart})
+        td = teacher.find_all('td')
+        name = td[1].text.strip()
+        names.append(name)
+        designation = td[3].text.strip()
+        designations.append(designation)
+        phone = td[6].text.strip()
+        phones.append(phone)
+        email = td[5].text.strip()
+        emails.append(email)
+        dept = td[4].text.strip()
+        depts.append(dept)
+    data = pd.DataFrame({'Name': names, 'Designation': designations, 'Phone': phones,
+                         'Email': emails, 'Department': depts})
     return data
-
+ 
 def main():
-    st.title("RUET Teachers Information")
-    #Department Selection
-    depts = ["CSE", "EEE", "CHEM", "MATH", "PHY"]
-    dept = st.sidebar.selectbox("Select Department", depts).lower()
-    
-    #Filter by Position using checkbox using elif
-    professor = st.sidebar.checkbox("Professor")
-    associate_professor = st.sidebar.checkbox("Associate Professor")
-    assistant_professor = st.sidebar.checkbox("Assistant Professor")
-    lecturer = st.sidebar.checkbox("Lecturer")
-
-    position = []
-    if professor:
-        position.append("Professor")
-    elif associate_professor:
-        position.append("Associate Professor")
-    elif assistant_professor:
-        position.append("Assistant Professor")
-    elif lecturer:
-        position.append("Lecturer")
-
+    st.title("RUET teachers information")
+    depts = ['EEE', 'CSE', 'CHEM', 'MATH', 'PHY', 'CHEM']
+    dept = st.sidebar.selectbox('Select Department', depts).lower()
+    prof = st.sidebar.checkbox('Professor', value=True)
+    assistant_prof = st.sidebar.checkbox('Assistant Professor', value=True)
+    associate_prof = st.sidebar.checkbox('Associate Professor', value=True)
+    lecturer = st.sidebar.checkbox('Lecturer', value=True)
+ 
     if dept:
         data = get_data(dept)
-    if position:
-            data = data[data['Designation'].isin(position)]
-    st.dataframe(data)
-
-#Constructor
-if __name__ == '__main__':
-    main()
+        cp = data.copy()
+ 
+        if (prof is False):
+            cp = cp[cp['Designation'] != 'Professor']
+ 
+        if (assistant_prof is False):
+            cp = cp[cp['Designation'] != 'Assistant Professor']
+ 
+        if (associate_prof is False):
+            cp = cp[cp['Designation'] != 'Associate Professor']
+ 
+        if (lecturer is False):
+            cp = cp[cp['Designation'] != 'Lecturer']
+ 
+        st.dataframe(cp)
+ 
+main()
